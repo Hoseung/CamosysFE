6f6583e42a66da9ed01a005f8b03cd927c20d069
Hoseung Choi <hschoi@dinsight.ai>
2024-08-28 22:05:57 +0900

    shortened with helper functions

--- main1.py
+++ main1.py
@@ -37,7 +37,7 @@ style_105_20 = """
             """
 style_255_40 = """
             color: rgb(255, 255, 255);
-            font-size: 40px;
+            font-size: 60px;
             font-weight: bold;
             """
 style_255_20 = """
@@ -64,9 +64,6 @@ class MainWindow(QWidget):
         self.fhd_shift_y = 56
         self.draw_conf_thr = 50
         
-
-        # self.post = Postprocess()
-
         self.setWindowTitle("Ti DEMO")
         self.setGeometry(100, 100, 1920, 1080)
         # 윈도우 프레임영역 제거하려면 아래 주석 제거
@@ -83,7 +80,7 @@ class MainWindow(QWidget):
         vbox2_1.setSpacing(0)
 
         vbox2_1_1 = QVBoxLayout()
-        vbox2_1_1.setContentsMargins(30, 30, 30, 30)
+        vbox2_1_1.setContentsMargins(30, 120, 30, 120)
 
         cont2_1_1 = QWidget()
         cont2_1_1.setStyleSheet("""
@@ -95,68 +92,24 @@ class MainWindow(QWidget):
         vbox2_1_1_1.setSpacing(0)
         cont2_1_1.setLayout(vbox2_1_1_1)
 
-        lbl_txt_2_1_1_1 = QLabel("Distance")
-        lbl_txt_2_1_1_1.setAlignment(Qt.AlignBottom)
-        lbl_txt_2_1_1_1.setContentsMargins(20, 0, 0, 0)
-        lbl_txt_2_1_1_1.setStyleSheet(
-            style_105_20
-        )
-        vbox2_1_1_1.addWidget(lbl_txt_2_1_1_1)
-        self.lbl_txt_2_1_1_2 = QLabel("100cm")
-        self.lbl_txt_2_1_1_2.setAlignment(Qt.AlignTop)
-        self.lbl_txt_2_1_1_2.setContentsMargins(20, 0, 0, 0)
-        self.lbl_txt_2_1_1_2.setStyleSheet(
-            style_255_40
-        )
-        vbox2_1_1_1.addWidget(self.lbl_txt_2_1_1_2)
-        lbl_txt_2_1_1_3 = QLabel("Eye Openness")
-        lbl_txt_2_1_1_3.setAlignment(Qt.AlignBottom)
-        lbl_txt_2_1_1_3.setContentsMargins(20, 0, 0, 0)
-        lbl_txt_2_1_1_3.setStyleSheet(
-            style_105_20
-        )
-        vbox2_1_1_1.addWidget(lbl_txt_2_1_1_3)
-        self.lbl_txt_2_1_1_4 = QLabel("64%")
-        self.lbl_txt_2_1_1_4.setAlignment(Qt.AlignTop)
-        self.lbl_txt_2_1_1_4.setContentsMargins(20, 0, 0, 0)
-        self.lbl_txt_2_1_1_4.setStyleSheet(
-            style_255_40
-        )
-        vbox2_1_1_1.addWidget(self.lbl_txt_2_1_1_4)
-        lbl_txt_2_1_1_5 = QLabel("Body Size")
-        lbl_txt_2_1_1_5.setAlignment(Qt.AlignBottom)
-        lbl_txt_2_1_1_5.setContentsMargins(20, 0, 0, 0)
-        lbl_txt_2_1_1_5.setStyleSheet(
-            style_105_20
-        )
-        vbox2_1_1_1.addWidget(lbl_txt_2_1_1_5)
-        self.lbl_txt_2_1_1_6 = QLabel("146cm")
-        self.lbl_txt_2_1_1_6.setAlignment(Qt.AlignTop)
-        self.lbl_txt_2_1_1_6.setContentsMargins(20, 0, 0, 0)
-        self.lbl_txt_2_1_1_6.setStyleSheet(
-            style_255_40
-        )
-        vbox2_1_1_1.addWidget(self.lbl_txt_2_1_1_6)
+        def add_label_to_layout(layout, text, alignment, margins, style):
+            label = QLabel(text)
+            label.setAlignment(alignment)
+            label.setContentsMargins(*margins)
+            label.setStyleSheet(style)
+            layout.addWidget(label)
+            return label
+
+        # Add the labels to the layout
+        add_label_to_layout(vbox2_1_1_1, "Distance", Qt.AlignBottom, (20, 0, 0, 0), style_105_20)
+        self.lbl_txt_2_1_1_2 = add_label_to_layout(vbox2_1_1_1, "100cm", Qt.AlignTop, (20, 0, 0, 0), style_255_40)
+        add_label_to_layout(vbox2_1_1_1, "Eye Openness", Qt.AlignBottom, (20, 0, 0, 0), style_105_20)
+        self.lbl_txt_2_1_1_4 = add_label_to_layout(vbox2_1_1_1, "64%", Qt.AlignTop, (20, 0, 0, 0), style_255_40)
+        add_label_to_layout(vbox2_1_1_1, "Body Size", Qt.AlignBottom, (20, 0, 0, 0), style_105_20)
+        self.lbl_txt_2_1_1_6 = add_label_to_layout(vbox2_1_1_1, "146cm", Qt.AlignTop, (20, 0, 0, 0), style_255_40)
 
         vbox2_1.addLayout(vbox2_1_1, stretch=5)
         vbox2_1_1.addWidget(cont2_1_1)
-
-        vbox2_1_2 = QVBoxLayout()
-        self.lbl_img_2_1_2 = QLabel()
-        self.lbl_img_2_1_2.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.lbl_img_2_1_2.setAlignment(Qt.AlignCenter)
-        # img2_1_2_1 = QPixmap('icon/Drowsiness-1ON.png')
-        # # img2_1_2_2 = QPixmap('icon/Property 1=0, Selected=On.png')
-        # self.lbl_img_2_1_2.setPixmap(img2_1_2_1)
-        vbox2_1_2.addWidget(self.lbl_img_2_1_2, stretch=3)
-        # lbl_txt_2_1_2 = QLabel("Drowsiness")
-        # lbl_txt_2_1_2.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        # lbl_txt_2_1_2.setAlignment(Qt.AlignHCenter)
-        # lbl_txt_2_1_2.setStyleSheet(
-        #     style_255_20
-        # )
-        # vbox2_1_2.addWidget(lbl_txt_2_1_2, stretch=1)
-        vbox2_1.addLayout(vbox2_1_2, stretch=3)
 
         self.img_lbl = QLabel()
         self.img_lbl.setAlignment(Qt.AlignCenter)
@@ -167,104 +120,46 @@ class MainWindow(QWidget):
         hbox2.addLayout(vbox2_1, stretch=2)
         hbox2.addLayout(vbox2_2, stretch=9)
 
+        def create_label_vbox(img_path, text, text_style):
+            vbox = QVBoxLayout()
+
+            lbl_img = QLabel()
+            lbl_img.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+            lbl_img.setAlignment(Qt.AlignCenter)
+            lbl_img.setPixmap(QPixmap(img_path))
+            vbox.addWidget(lbl_img, stretch=3)
+
+            lbl_txt = QLabel(text)
+            lbl_txt.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+            lbl_txt.setAlignment(Qt.AlignHCenter)
+            lbl_txt.setStyleSheet(text_style)
+            vbox.addWidget(lbl_txt, stretch=1)
+
+            return vbox, lbl_img, lbl_txt
+
         hbox3 = QHBoxLayout()
         hbox3.setSpacing(0)
 
-        vbox3_1 = QVBoxLayout()
-        self.lbl_img_3_1 = QLabel()
-        self.lbl_img_3_1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.lbl_img_3_1.setAlignment(Qt.AlignCenter)
-        img3_1_1 = QPixmap('icon/PhoneUseOff.png')
-        self.lbl_img_3_1.setPixmap(img3_1_1)
-        vbox3_1.addWidget(self.lbl_img_3_1, stretch=3)
-        self.lbl_txt_3_1 = QLabel("Phone use (90%)")
-        self.lbl_txt_3_1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.lbl_txt_3_1.setAlignment(Qt.AlignHCenter)
-        self.lbl_txt_3_1.setStyleSheet(
-            style_255_20
-        )
-        vbox3_1.addWidget(self.lbl_txt_3_1, stretch=1)
-        hbox3.addLayout(vbox3_1, stretch=10)
+        # List of tuples containing image path, text, and stylesheet
+        label_data = [
+            ('icon/PhoneUseOff.png', 'Phone use (90%)', style_255_20),
+            ('icon/PassengerEmptyOff.png', 'Empty', style_105_20),
+            ('icon/PassengerKidsOff.png', 'Age 1~6', style_105_20),
+            ('icon/PassengerAF05Off.png', 'AF05', style_105_20),
+            ('icon/PassengerAM50Off.png', 'AM50', style_105_20),
+            ('icon/PassengerAM95Off.png', 'AM95', style_105_20),
+        ]
 
-        vbox3_2 = QVBoxLayout()
-        self.lbl_img_3_2 = QLabel()
-        self.lbl_img_3_2.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.lbl_img_3_2.setAlignment(Qt.AlignCenter)
-        img3_2_1 = QPixmap('icon/PassengerEmptyOff.png')
-        self.lbl_img_3_2.setPixmap(img3_2_1)
-        vbox3_2.addWidget(self.lbl_img_3_2, stretch=3)
-        self.lbl_txt_3_2 = QLabel("Empty")
-        self.lbl_txt_3_2.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.lbl_txt_3_2.setAlignment(Qt.AlignHCenter)
-        self.lbl_txt_3_2.setStyleSheet(
-            style_105_20
-        )
-        vbox3_2.addWidget(self.lbl_txt_3_2, stretch=1)
-        hbox3.addLayout(vbox3_2, stretch=9)
+        # Initialize a list to keep track of the text labels
+        self.lbl_txt_list = []
+        self.lbl_img_list = []
 
-        vbox3_3 = QVBoxLayout()
-        self.lbl_img_3_3 = QLabel()
-        self.lbl_img_3_3.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.lbl_img_3_3.setAlignment(Qt.AlignCenter)
-        img3_3_1 = QPixmap('icon/PassengerKidsOff.png')
-        self.lbl_img_3_3.setPixmap(img3_3_1)
-        vbox3_3.addWidget(self.lbl_img_3_3, stretch=3)
-        self.lbl_txt_3_3 = QLabel("Age 1~6")
-        self.lbl_txt_3_3.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.lbl_txt_3_3.setAlignment(Qt.AlignHCenter)
-        self.lbl_txt_3_3.setStyleSheet(
-            style_105_20
-        )
-        vbox3_3.addWidget(self.lbl_txt_3_3, stretch=1)
-        hbox3.addLayout(vbox3_3, stretch=9)
-
-        vbox3_4 = QVBoxLayout()
-        self.lbl_img_3_4 = QLabel()
-        self.lbl_img_3_4.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.lbl_img_3_4.setAlignment(Qt.AlignCenter)
-        img3_4_1 = QPixmap('icon/PassengerAF05Off.png')
-        self.lbl_img_3_4.setPixmap(img3_4_1)
-        vbox3_4.addWidget(self.lbl_img_3_4, stretch=3)
-        self.lbl_txt_3_4 = QLabel("AF05")
-        self.lbl_txt_3_4.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.lbl_txt_3_4.setAlignment(Qt.AlignHCenter)
-        self.lbl_txt_3_4.setStyleSheet(
-            style_105_20
-        )
-        vbox3_4.addWidget(self.lbl_txt_3_4, stretch=1)
-        hbox3.addLayout(vbox3_4, stretch=9)
-
-        vbox3_5 = QVBoxLayout()
-        self.lbl_img_3_5 = QLabel()
-        self.lbl_img_3_5.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.lbl_img_3_5.setAlignment(Qt.AlignCenter)
-        img3_5_1 = QPixmap('icon/PassengerAM50Off.png')
-        self.lbl_img_3_5.setPixmap(img3_5_1)
-        vbox3_5.addWidget(self.lbl_img_3_5, stretch=3)
-        self.lbl_txt_3_5 = QLabel("AM50")
-        self.lbl_txt_3_5.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.lbl_txt_3_5.setAlignment(Qt.AlignHCenter)
-        self.lbl_txt_3_5.setStyleSheet(
-            style_105_20
-        )
-        vbox3_5.addWidget(self.lbl_txt_3_5, stretch=1)
-        hbox3.addLayout(vbox3_5, stretch=9)
-
-        vbox3_6 = QVBoxLayout()
-        self.lbl_img_3_6 = QLabel()
-        self.lbl_img_3_6.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.lbl_img_3_6.setAlignment(Qt.AlignCenter)
-        img3_6_1 = QPixmap('icon/PassengerAM95Off.png')
-        self.lbl_img_3_6.setPixmap(img3_6_1)
-        vbox3_6.addWidget(self.lbl_img_3_6, stretch=3)
-        self.lbl_txt_3_6 = QLabel("AM95")
-        self.lbl_txt_3_6.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.lbl_txt_3_6.setAlignment(Qt.AlignHCenter)
-        self.lbl_txt_3_6.setStyleSheet(
-            style_105_20
-        )
-        vbox3_6.addWidget(self.lbl_txt_3_6, stretch=1)
-        hbox3.addLayout(vbox3_6, stretch=9)
+        # Add each label's vbox to hbox3 and store the label in the list
+        for img_path, text, text_style in label_data:
+            vbox, lbl_img, lbl_txt = create_label_vbox(img_path, text, text_style)
+            hbox3.addLayout(vbox, stretch=9)
+            self.lbl_txt_list.append(lbl_txt)
+            self.lbl_img_list.append(lbl_img)
 
         vboxmain.addLayout(hbox2, stretch=8)
         vboxmain.addLayout(hbox3, stretch=3)
@@ -303,15 +198,6 @@ class MainWindow(QWidget):
                                   int(self.frame_height_resize)), 
                           (105, 105, 105), 5)
             
-            # # Face bounding box
-            # bbox = label_data["face_bounding_box"][0]
-            # if bbox is not None and bbox[0] > area_lmin and bbox[3] < area_rmax:
-            #     if self.box_old is None:
-            #         self.box_old = bbox
-            #     bbox = self.draw_alpha * bbox + (1 - self.draw_alpha) * self.box_old
-            #     self.box_old = bbox
-            #     # FHD 이미지를 resize 할때 쓴 ratio를 얻었으므로, 
-            #     # 좌표로 FHD 기준으로 바꿔준 뒤 ratio 사용. 
             if all(label_data["body_keypoints2d"][0][0] != -1):
                 # body_keypoints
                 bk2d_x = np.round((label_data["body_keypoints2d"][0][0] + self.fhd_shift_x) * frame_width_resize_ratio).astype(int)
@@ -356,13 +242,7 @@ class MainWindow(QWidget):
                     self.box_old = bbox
                 bbox = self.draw_alpha * bbox + (1 - self.draw_alpha) * self.box_old
                 self.box_old = bbox
-                # FHD 이미지를 resize 할때 쓴 ratio를 얻었으므로, 
-                # 좌표로 FHD 기준으로 바꿔준 뒤 ratio 사용. 
-                # face_landmarks
                 
-            
-                # print("sss", f'icon/Drowsiness{str(label_data["drowsiness"][0])}')
-                # self.lbl_img_2_1_2.setPixmap(QPixmap(f'icon/Drowsiness{str(label_data["drowsiness"][0])}ON.png'))
             if not label_data["face_landmarks_x"][0][0] == -1:
                 face_landmarks_x = np.round((label_data["face_landmarks_x"][0] + self.fhd_shift_x) * frame_width_resize_ratio).astype(int)
                 face_landmarks_y = np.round((label_data["face_landmarks_y"][0] + self.fhd_shift_y) * frame_height_resize_ratio).astype(int)
@@ -387,12 +267,14 @@ class MainWindow(QWidget):
                 _phone_use = 1 if label_data["phoneuse"][0] == 1 else 0
                 self.phone_use = self.draw_alpha * _phone_use + (1 - self.draw_alpha) * self.phone_use
                 is_using_phone = self.phone_use > 0.8
-                self.lbl_img_3_1.setPixmap(QPixmap(f'icon/PhoneUse{"On" if is_using_phone else "Off"}.png'))
+                self.lbl_txt_list[0].setPixmap(QPixmap(f'icon/PhoneUse{"On" if is_using_phone else "Off"}.png'))
+                # self.lbl_img_3_1.setPixmap(QPixmap(f'icon/PhoneUse{"On" if is_using_phone else "Off"}.png'))
                 if is_using_phone:
                     phone_conf = label_data["phone_use_conf"][0]
                 else:
                     phone_conf = 0
-                self.lbl_txt_3_1.setText("Phone use (" + str(round(phone_conf,2)) + "%)")                  
+                # self.lbl_txt_3_1.
+                self.lbl_txt_list[0].setText("Phone use (" + str(round(phone_conf,2)) + "%)")                  
                 
             qimg = QImage(frame.data, w, h, bytes_per_line, QImage.Format_RGB888).rgbSwapped()
             #qimg = QImage(frame.data, w, h, bytes_per_line, QImage.Format_Grayscale8).rgbSwapped()
@@ -411,77 +293,34 @@ class MainWindow(QWidget):
                 color = "rgb(255,255,255)"
 
             # Set the color using a stylesheet
-            self.lbl_txt_2_1_1_2.setStyleSheet(f"color: {color}; font-size: 40px; font-weight: bold;")
+            self.lbl_txt_2_1_1_2.setStyleSheet(f"color: {color}; font-size: 60px; font-weight: bold;")
             self.lbl_txt_2_1_1_6.setText(str(label_data['height'][0]))
             # bodysize
                         
+            self.update_labels(label_data["passenger"][0])
                 
-            self.lbl_img_3_2.setPixmap(QPixmap(f'icon/PassengerEmpty{"On" if label_data["passenger"][0] == 0 else "Off"}.png'))
-            self.lbl_txt_3_2.setStyleSheet(
-                f"""
-                {"color: rgb(17, 94, 255);" if label_data["passenger"][0] == 0 else "color: rgb(105, 105, 105);"}
-                font-size: 20px;
-                font-weight: bold;
-                """
-            )
-            self.lbl_img_3_3.setPixmap(QPixmap(f'icon/PassengerKids{"On" if label_data["passenger"][0] == 1 else "Off"}.png'))
-            self.lbl_txt_3_3.setStyleSheet(
-                f"""
-                            {"color: rgb(17, 94, 255);" if label_data["passenger"][0] == 1 else "color: rgb(105, 105, 105);"}
-                            font-size: 20px;
-                            font-weight: bold;
-                            """
-            )
+    def set_label(self, label_img, label_txt, icon_name, condition):
+        label_img.setPixmap(QPixmap(f'icon/{icon_name}{"On" if condition else "Off"}.png'))
+        label_txt.setStyleSheet(
+            f"""
+            {"color: rgb(17, 94, 255);" if condition else "color: rgb(105, 105, 105);"}
+            font-size: 20px;
+            font-weight: bold;
+            """
+        )
 
-            self.lbl_img_3_4.setPixmap(QPixmap(f'icon/PassengerAF05{"On" if label_data["passenger"][0] == 2 else "Off"}.png'))
-            self.lbl_txt_3_4.setStyleSheet(
-                f"""
-                            {"color: rgb(17, 94, 255);" if label_data["passenger"][0] == 2 else "color: rgb(105, 105, 105);"}
-                            font-size: 20px;
-                            font-weight: bold;
-                            """
-            )
-
-            self.lbl_img_3_5.setPixmap(QPixmap(f'icon/PassengerAM50{"On" if label_data["passenger"][0] == 3 else "Off"}.png'))
-            self.lbl_txt_3_5.setStyleSheet(
-                f"""
-                            {"color: rgb(17, 94, 255);" if label_data["passenger"][0] == 3 else "color: rgb(105, 105, 105);"}
-                            font-size: 20px;
-                            font-weight: bold;
-                            """
-            )
-
-            self.lbl_img_3_6.setPixmap(QPixmap(f'icon/PassengerAM95{"On" if label_data["passenger"][0] == 4 else "Off"}.png'))
-            self.lbl_txt_3_6.setStyleSheet(
-                f"""
-                            {"color: rgb(17, 94, 255);" if label_data["passenger"][0] == 4 else "color: rgb(105, 105, 105);"}
-                            font-size: 20px;
-                            font-weight: bold;
-                            """
-            )
-
-    def closeEvent(self, event):
-        self.client.running = False
-        self.client_thread.join()
-        event.accept()
-
-    def switch_image(self, lbl_img, lbl_txt, img1, img2, flag):
-        if flag[0] == 0:
-            lbl_img.setPixmap(img1)
-            lbl_txt.setStyleSheet(
-                style_105_20
-            )
-            flag[0] = 1
-        else:
-            lbl_img.setPixmap(img2)
-            lbl_txt.setStyleSheet(
-                """
-                color: rgb(17, 94, 255);
-                font-size: 20px;
-                font-weight: bold;
-                """
-            )
-            flag[0] = 0
+    def update_labels(self, passenger_status):
+        labels_info = [
+            ('PassengerEmpty',passenger_status == 0),
+            ('PassengerKids', passenger_status == 1),
+            ('PassengerAF05', passenger_status == 2),
+            ('PassengerAM50', passenger_status == 3),
+            ('PassengerAM95', passenger_status == 4),
+        ]
+        
+        for label_img, label_txt, (icon_name, condition) in zip(self.lbl_img_list[1:], 
+                                                                self.lbl_img_list[1:], labels_info):
+            self.set_label(label_img, label_txt, icon_name, condition)
 
 def main():
     parser=argparse.ArgumentParser()
@@ -500,7 +339,7 @@ def main():
     client = Client(server_ip = host_ip, 
                     camera_height=args.height, 
                     camera_pitch=args.pitch,
-                    height_factor=1.17) #
+                    height_factor=1.0) #
     # client.setup_socket()
     # client.accept_connection()
 
